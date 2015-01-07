9fbd403650c7e02c8267cd51f62b7be8cadda5f2
Bruno Lemos <brunohplemos@gmail.com>
2015-01-07 16:30:45 -0200

    Common Utilities

---
+++ Hooks/Hooks.Shared/Common/EmptyListToVisibilityConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace Hooks.Common
+{
+    public sealed class EmptyListToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return (value == null || (value as IList).Count <= 0) ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Hooks/Hooks.Shared/Common/ListToVisibilityConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace Hooks.Common
+{
+    public sealed class ListToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return (value == null || (value as IList).Count <= 0) ? Visibility.Collapsed : Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
