252611e63f038ea3b6a35f3badd03061886cd92b
Bruno Lemos <brunohplemos@gmail.com>
2015-01-07 16:31:02 -0200

    Models

---
+++ Hooks/Hooks.Shared/Models/Hook.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Runtime.Serialization;
+using Hooks.Common;
+
+namespace Hooks.Models
+{
+    [DataContract]
+    public class Hook
+    {
+        [DataMember(Name = "_id")]
+        public String ID { get; set; }
+
+        [DataMember(Name = "namespace")]
+        public String Namespace { get; set; }
+
+        [DataMember(Name = "approved")]
+        public bool Approved { get; set; }
+
+        [DataMember(Name = "removed")]
+        public bool Disabled { get; set; }
+
+        public bool Enabled { get { return !Disabled; } set { Disabled = !value; } }
+    }
+    
+    public class HookList : ObservableCollection<Hook> { }
+}
---
+++ Hooks/Hooks.Shared/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace Hooks.Models
+{
+    [DataContract]
+    public class User
+    {
+        [DataMember(Name = "_id")]
+        public String ID { get; set; }
+
+        [DataMember(Name = "apiSecret")]
+        public String ApiSecret { get; set; }
+
+        [DataMember(Name = "email")]
+        public String Email { get; set; }
+
+        [DataMember(Name = "namespace")]
+        public List<String> Namespaces { get; set; }
+    }
+}
